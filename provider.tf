20526ec78c654718dd86b3b44c342e3a10f3049a
flizzar <124061637+flizzar@users.noreply.github.com>
2023-03-27 16:19:24 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
-  version         = "2.99"
+  version         = "3.7.0"
+  use_oidc = true
   features {}
 }
 
@@ -7,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.99"
+      version = "3.7.0"
     }
   }
   backend "azurerm" {
